65864f133c37733ffdad0c23acb324c6fc4ce7ce
Faso312 <79243423+Faso312@users.noreply.github.com>
2024-02-28 16:08:12 +0000

    -

--- handlers/admin.py
+++ handlers/admin.py
@@ -2,7 +2,7 @@ from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 from aiogram import Router, F, types
-from .db import get_vote_results, clear_sheets, available_departmemts, key
+from .db import get_results, clear_sheets, available_departmemts, key
 from .run import update_cand_list as update_
 from keyboards.dinemic_kb import make_row_keyboard
 from keyboards.static_kb import get_admin_kb
@@ -10,7 +10,7 @@ from keyboards.static_kb import get_admin_kb
 
 router = Router()
 
-class navigate(StatesGroup):
+class Navigate(StatesGroup):
     insert_key=State()
     choose_department=State()
 
@@ -19,9 +19,9 @@ class navigate(StatesGroup):
 async def admin_kb(callback: types.CallbackQuery, state: FSMContext): # клавиатура админа
     await state.clear()  
     await callback.message.answer(f"Введите секретный ключ", reply_markup=ReplyKeyboardRemove())
-    await state.set_state(navigate.insert_key)
+    await state.set_state(Navigate.insert_key)
 
-@router.message(navigate.insert_key,F.text)
+@router.message(Navigate.insert_key,F.text)
 async def try_key(message: Message, state: FSMContext): # проверка ключа
     if message.text == key:
         await message.answer(f'Функции администратора:', reply_markup=get_admin_kb())
@@ -33,16 +33,16 @@ async def try_key(message: Message, state: FSMContext): # проверка кл�
 async def running(callback: types.CallbackQuery, state: FSMContext): 
     try:
         await callback.message.answer(f'Выберите кафедру',reply_markup=make_row_keyboard(available_departmemts))
-        await state.set_state(navigate.choose_department) #Устанавливаем пользователю состояние "выбирает ответ"
+        await state.set_state(Navigate.choose_department) #Устанавливаем пользователю состояние "выбирает ответ"
         await callback.answer() 
     except Exception as e:  await callback.message.answer(f'Ошибка: {e}, Обратитесь к админитстратору')
     
 
-@router.message(navigate.choose_department, F.text.in_(available_departmemts))
+@router.message(Navigate.choose_department, F.text.in_(available_departmemts))
 async def setting_departmant(message: Message, state: FSMContext):  #получаем результаты голосования
     await state.update_data(department=message.text)
     user_data = await state.get_data()   
-    results=get_vote_results(int(available_departmemts.index(user_data['department'])))
+    results=get_results(int(available_departmemts.index(user_data['department'])))
     await message.answer(f"🎉Поздарвляем, {results[0]} с каферды {user_data['department']}🎉 \nЗа этого кандидата проголосовало {results[1]} человек", reply_markup=ReplyKeyboardRemove())
     await state.clear() 
 # _________________________________________results
@@ -62,10 +62,10 @@ async def admin_kb(callback: types.CallbackQuery, state: FSMContext): # клав
 @router.callback_query(F.data == 'clear_sheet')
 async def admin_kb(callback: types.CallbackQuery, state: FSMContext): # клавиатура админа
     await callback.message.answer(f'Выберите кафедру', reply_markup=make_row_keyboard(available_departmemts))
-    await state.set_state(navigate.choose_department) #Устанавливаем пользователю состояние "выбирает ответ"
+    await state.set_state(Navigate.choose_department) #Устанавливаем пользователю состояние "выбирает ответ"
     await callback.answer() 
 
-@router.message(navigate.choose_department, F.text.in_(available_departmemts))
+@router.message(Navigate.choose_department, F.text.in_(available_departmemts))
 async def setting_departmant(message: Message, state: FSMContext):  #получаем результаты голосования
     user_data = await state.get_data()   
     clear_sheets(int(available_departmemts.index(user_data['department'])))
--- handlers/db.py
+++ handlers/db.py
@@ -11,13 +11,13 @@ key='00000'
 
 def on_hold(sec: int): time.sleep(sec) # функция задержки 
 
-def check(user_id: str,department_id: int): #проверка на наличие id пользователя в системе
+def check(user_id: str,dp_id: int): #проверка на наличие id пользователя в системе
     try:
-        if sh.get_worksheet(department_id+3).find(user_id) is None: return True
+        if sh.get_worksheet(dp_id+3).find(user_id) is None: return True
         else: return False
     except gspread.exceptions.APIError:
         on_hold(5)
-        return check(user_id,department_id)
+        return check(user_id,dp_id)
 
 def get_contenders():
     try:
@@ -27,45 +27,34 @@ def get_contenders():
         on_hold(5)
         return get_contenders()
 
-def pass_user_data(user_id: str,department_id: int,myList: list): #принимаем id пользователя и список ответов
+def pass_user_data(user_id: str,dp_id: int,myList: list): #принимаем id пользователя и список ответов
     try:
-        worksheet=sh.get_worksheet(department_id+3) #определение рабочей страницы в таблице
-        last_row = len(worksheet.get_all_values()) + 1 #получение последнего значения заполненной строки +1 
-        if check(user_id,  department_id) is True: #проверям на наличие id в таблице
+        worksheet=sh.get_worksheet(dp_id+3) #определение рабочей страницы в таблице
+        last_row = len(worksheet.get_values()) + 1 #получение последнего значения заполненной строки +1 
+        if check(user_id,  dp_id) is True: #проверям на наличие id в таблице
             myList.insert(0,user_id) # Добавляем id на первое место в списке
             for col in range(1, len(myList)+1,1): #заполнение через for(1, длинна списка ответов, шаг 1)
                 worksheet.update_cell(last_row, col, myList[col-1]) #определяем место ввода(поселдняя свободная, столбец, значение)
         else: return False 
     except gspread.exceptions.APIError:
         on_hold(5)
-        return pass_user_data(user_id,department_id,myList)
-
-def get_vote_results(department_id: int) -> list: 
-    try:
-        vote_list=[] #создаем локальный список голосов
-        worksheet=sh.get_worksheet(department_id+3) #определение рабочей страницы в таблице   
-        for itr in range(2,26,1): #перебор столбцов
-            votes=worksheet.col_values(itr) #определение 
-            if votes: #bool  проверка на пустые столбцы
-                candidate=votes[0] #вопрос(имя кандидата)
-                votes_for=votes.count('за') #голоса за 
-                vote_list.extend([candidate,votes_for]) #подставление значений в список
-        max_Value=max([vote_list[itr] for itr in range(1, len(vote_list),2)]) #получаем маскимальное значения
-        winner_value=vote_list[int(vote_list.index(max_Value))-1] #получаем имя победителя
-        return [winner_value,max_Value] #возвращаем список(имя-за)
-    except gspread.exceptions.APIError:
-        on_hold(5)
-        return get_vote_results(department_id)
+        return pass_user_data(user_id,dp_id,myList)
     
-def get_results(dp_id: int) -> list[list]: 
-    t=sh.get_worksheet(dp_id+3)
-    print(t)
-    worksheet=np.array(t).T #определение рабочей страницы в таблице   
-    print(worksheet)
-get_results(1)
-def clear_sheets(department_id: int): #очищает клетки в диапозоне
-    try: 
-        sh.get_worksheet(department_id+3).batch_clear(["A3:Z100"]) #определяем область очистки
+def get_results(dp_id: int) -> list: #результаты голосования
+    try:
+        list_=np.array(sh.get_worksheet(dp_id+3).get_values()).T[1:] #список кандидатов и результаты
+        result_list=np.array([[item[0],np.count_nonzero(item=='за')] for item in list_]) #список кандидатов и голоса за
+        max_index = np.argmax(result_list[:, 1]) #индекс списка с максимальным числом голосов
+        return result_list[max_index] #вывод списка по индексу
     except gspread.exceptions.APIError: 
         on_hold(5)
-        return clear_sheets(department_id)
+        return get_results(dp_id)
+    
+print(get_results(1))
+
+def clear_sheets(dp_id: int): #очищает клетки в диапозоне
+    try: 
+        sh.get_worksheet(dp_id+3).batch_clear(["A3:Z100"]) #определяем область очистки
+    except gspread.exceptions.APIError: 
+        on_hold(5)
+        return clear_sheets(dp_id)
